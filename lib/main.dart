aaaf4d78e94cd9f8777db45b2dfd050527bd3757
samanthagc <samgomescosta@gmail.com>
2025-03-09 12:11:02 -0300

    feat: includes basic widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyWidget());
+  runApp(const MaterialApp(home: MyApp()));
 }
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  TextStyle textStyle() {
+    return TextStyle(
+      fontSize: 40, 
+      color: Colors.black,
+      fontStyle: FontStyle.italic,
+      fontWeight: FontWeight.bold,
+      decoration: TextDecoration.underline
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello Word!', textDirection: TextDirection.ltr),);
+    return Scaffold(
+      appBar: AppBar(title: Text('Primeiro App')),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 134, 139, 179),
+              border: Border.all(color: const Color.fromARGB(255, 48, 47, 94), width: 2),
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: Text('Texto 1', style: textStyle())
+          ),
+          ElevatedButton(onPressed: () {}, child: Text('Botão')),
+          ],
+      ),
+
+    ); 
   }
 }
